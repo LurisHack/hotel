bdf596c37a6409b2f6ccfda0b7bcd24eb819cbc2
LurisHack <luris5899@gmail.com>
2022-12-12 18:58:30 +0630

    luris commit

---
+++ src/app/testingComponent/testing/testing.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-component',
+  templateUrl: './testing.component.html',
+  styleUrls: ['./testing.component.scss'],
+})
+export class TestingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
