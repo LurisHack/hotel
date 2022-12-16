cd4740b0d1ca19b2ee9e605314df140be08cbec3
OP-11 <112000opq@gmail.com>
2022-12-16 21:59:40 +0630

    MC

--- src/app/admin/component/room/room.component.ts
+++ src/app/admin/component/room/room.component.ts
@@ -3,10 +3,11 @@ import {Store} from "@ngrx/store";
 import {RoomUIComponent} from "../../subComponent/room-ui/room-ui.component";
 import {ScrollingModule} from "@angular/cdk/scrolling";
 import {AdminComponentModule} from "../admin-component.module";
+import {RoomSkeletonComponent} from "../../skeletonComponent/room-skeleton/room-skeleton.component";
 
 @Component({
   standalone: true,
-  imports: [AdminComponentModule, RoomUIComponent, ScrollingModule],
+  imports: [AdminComponentModule, RoomUIComponent, ScrollingModule, RoomSkeletonComponent],
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.scss'],
@@ -16,11 +17,13 @@ export class ROOMComponent{
 
 
   scrollIndex = 0
-
+  loaded = true
 
   constructor(private store: Store<{testingValue: {testingValue: number[]}}>) {
 
-
+    setTimeout(()=>{
+      this.loaded = false
+    },5000)
 
   }
 
