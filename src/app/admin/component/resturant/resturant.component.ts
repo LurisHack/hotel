341ffca1ab10c7a3fce9595426628d1c5b1f55ae
Fujitsu <linhtetkyaw1478@gmail.com>
2022-12-18 13:22:24 +0630

    Robert gj

--- src/app/admin/component/resturant/resturant.component.ts
+++ src/app/admin/component/resturant/resturant.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {AdminComponentModule} from "../admin-component.module";
 import {RestaurantUIComponent} from "../../uiComponent/restaurant-ui/restaurant-ui.component";
+import {RestaurantSkeletonComponent} from "../../skeletonComponent/restaurant-skeleton/restaurant-skeleton.component";
 
 @Component({
   standalone: true,
-  imports: [AdminComponentModule,RestaurantUIComponent],
+  imports: [AdminComponentModule,RestaurantUIComponent,RestaurantSkeletonComponent],
   selector: 'app-resturant',
   templateUrl: './resturant.component.html',
   styleUrls: ['./resturant.component.scss'],
