8ff09d4c04fa6aeaa919b9958be52527537785fd
YuiKon11 <kc4433667@gmail.com>
2022-12-19 18:46:44 +0630

    YuiKon11

--- src/app/admin/component/store/store.component.ts
+++ src/app/admin/component/store/store.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {AdminComponentModule} from "../admin-component.module";
+import {StoreSkeletonComponent} from "../../skeletonComponent/store-skeleton/store-skeleton.component";
 
 @Component({
   standalone: true,
-  imports: [AdminComponentModule],
+  imports: [AdminComponentModule, StoreSkeletonComponent],
   selector: 'app-store',
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.scss'],
 })
 export class STOREComponent implements OnInit {
 
-  loaded = true
-  constructor() {
 
+  loaded = true
+
+
+  constructor() {
+    setTimeout(()=>{
+      this.loaded = false
+    },5000)
   }
 
   ngOnInit() {}
