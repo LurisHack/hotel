3420314a5b59e41af00e7a23d4305109e7853246
LurisHack <luris5899@gmail.com>
2022-12-20 22:07:43 +0630

    luris commit

---
+++ src/app/admin/uiComponent/playground-ui/playground-ui.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-playground-ui',
+  templateUrl: './playground-ui.component.html',
+  styleUrls: ['./playground-ui.component.scss'],
+})
+export class PlaygroundUIComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
