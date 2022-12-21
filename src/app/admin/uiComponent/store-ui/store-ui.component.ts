d882a27b7f197cff531c988bf3ecc46bf4bb7134
YuiKon11 <kc4433667@gmail.com>
2022-12-21 20:19:00 +0630

    YuiKon11

--- src/app/admin/uiComponent/store-ui/store-ui.component.ts
+++ src/app/admin/uiComponent/store-ui/store-ui.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {AdminComponentModule} from "../../component/admin-component.module";
+import {ScrollingModule} from "@angular/cdk/scrolling";
+import {RoomPopupComponent} from "../../popupComponent/room-popup/room-popup.component";
 
 @Component({
+  standalone: true,
+  imports: [AdminComponentModule,],
   selector: 'app-store-ui',
   templateUrl: './store-ui.component.html',
   styleUrls: ['./store-ui.component.scss'],
