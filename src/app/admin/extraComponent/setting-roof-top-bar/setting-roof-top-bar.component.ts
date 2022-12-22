668f7057c74e6456a9a2e2238b265151a819b3f9
Fujitsu <linhtetkyaw1478@gmail.com>
2022-12-22 18:38:10 +0630

    Robert GJ

--- src/app/admin/extraComponent/setting-roof-top-bar/setting-roof-top-bar.component.ts
+++ src/app/admin/extraComponent/setting-roof-top-bar/setting-roof-top-bar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {AdminComponentModule} from "../../component/admin-component.module";
 
 @Component({
+  standalone:true,
+  imports:[AdminComponentModule],
   selector: 'app-setting-roof-top-bar',
   templateUrl: './setting-roof-top-bar.component.html',
   styleUrls: ['./setting-roof-top-bar.component.scss'],
