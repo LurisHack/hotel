d73ece79dc9fc5d47d5ad1a98ddf0ff019c35c33
LurisHack <luris5899@gmail.com>
2022-12-23 08:35:00 +0630

    luris commit

--- src/app/admin/uiComponent/welcome-presentation-ui/welcome-presentation-ui.component.ts
+++ src/app/admin/uiComponent/welcome-presentation-ui/welcome-presentation-ui.component.ts
@@ -3,7 +3,7 @@ import {AdminComponentModule} from "../../component/admin-component.module";
 
 @Component({
   standalone: true,
-  selector: 'app-welcome-presentation-creation-ui',
+  selector: 'app-welcome-presentation-ui',
   templateUrl: './welcome-presentation-ui.component.html',
   styleUrls: ['./welcome-presentation-ui.component.scss'],
   imports: [
