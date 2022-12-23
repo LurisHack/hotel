e945ea8df06920ab80d992fa88f59cf68de42043
Fujitsu <linhtetkyaw1478@gmail.com>
2022-12-23 10:27:25 +0630

    ROBERTgj

--- src/app/admin/creationComponent/roof-top-bar-creation/setting-roof-top-bar.component.ts
+++ src/app/admin/creationComponent/roof-top-bar-creation/setting-roof-top-bar.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {AdminComponentModule} from "../../component/admin-component.module";
+import {ActivatedRoute, RouterLink} from "@angular/router";
+import {SiteInformationService} from "../../../utility/service/siteInformation.service";
+import {RoomFilterPipe} from "../../../utility/pipe/room-filter.pipe";
 
 @Component({
   standalone:true,
-  imports:[AdminComponentModule],
+  imports: [AdminComponentModule, RouterLink, RoomFilterPipe],
   selector: 'app-roof-top-bar-creation',
   templateUrl: './setting-roof-top-bar.component.html',
   styleUrls: ['./setting-roof-top-bar.component.scss'],
 })
 export class SettingRoofTopBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public activatedRoute: ActivatedRoute,
+    public siteInformationService: SiteInformationService
+  ) { }
 
   ngOnInit() {}
 
+  SettingRoofTopBar() {
+    this.siteInformationService.addRoomName(this.activatedRoute.snapshot.params['id'])
+  }
 }
