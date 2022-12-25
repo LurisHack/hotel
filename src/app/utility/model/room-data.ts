02098121bcf9074f364edcf687fb6171d5f19221
LurisHack <luris5899@gmail.com>
2022-12-25 08:36:22 +0630

    luris commit

---
+++ src/app/utility/model/room-data.ts
@@ -0,0 +1,31 @@
+export interface RoomDataModel {
+  buildingData: {
+    id: string;
+    name: string;
+  }
+  id: string;
+  name: string;
+  roomState: string;
+  roomType: {
+    id: string;
+    name: string;
+  }
+  stayData: {
+    stayTimeLength:  {
+      roomTypeId: string;
+      packageType: string;
+      price: number;
+      hour?: number;
+      startHour?: any;
+      endHour?: any;
+    }[],
+  },
+  usedData: {
+    store: any[];
+    restaurant: any[];
+    foc: any[];
+    damage: any[]
+  },
+  startTime: number;
+  endTime: number
+}
