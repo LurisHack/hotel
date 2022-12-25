653f66e9d221d1ecdf5465f6c51d76618f11f12b
OP-11 <112000opq@gmail.com>
2022-12-25 13:20:46 +0630

    FLASH

--- src/app/admin/popupComponent/resturant-add-food-popup-component/restaurant-add-food-popup-component.component.ts
+++ src/app/admin/popupComponent/resturant-add-food-popup-component/restaurant-add-food-popup-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AdminComponentModule} from "../../component/admin-component.module";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   standalone: true,
@@ -7,13 +8,29 @@ import {AdminComponentModule} from "../../component/admin-component.module";
   templateUrl: './restaurant-add-food-popup-component.component.html',
   styleUrls: ['./restaurant-add-food-popup-component.component.scss'],
   imports: [
-    AdminComponentModule
+    AdminComponentModule,
+    ReactiveFormsModule,
   ]
 })
 export class RestaurantAddFoodPopupComponentComponent implements OnInit {
 
+  formGroup : any;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
 
+    this.formGroup = new FormGroup<any>({
+      name: new FormControl(null, Validators.required),
+      price: new FormControl(null, Validators.required),
+      count: new FormControl(null, Validators.required),
+    })
+  }
+
+
+
+
+  addRestaurantFood() {
+    console.log(this.formGroup)
+  }
 }
