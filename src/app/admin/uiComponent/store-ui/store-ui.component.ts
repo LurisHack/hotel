6d33553e635bb2ea85824166609eb6c1574e57c2
YuiKon11 <kc4433667@gmail.com>
2022-12-25 13:39:26 +0630

    YuiKon11

--- src/app/admin/uiComponent/store-ui/store-ui.component.ts
+++ src/app/admin/uiComponent/store-ui/store-ui.component.ts
@@ -13,6 +13,14 @@ import {StoreAddProductComponent} from "../../popupComponent/store-add-product/s
   styleUrls: ['./store-ui.component.scss'],
 })
 export class StoreUiComponent implements OnInit {
+  propertyName = [
+    {no: 1,name: 'RRQ',price: 2000, count: 1, total: 2000},
+    {no: 2,name: 'Apex',price: 500, count: 2, total: 1000},
+    {no: 3,name: 'cola',price: 800, count: 3, total: 2400},
+    {no: 4,name: 'juice',price: 1500, count: 5, total: 7500},
+    {no: 5,name: 'spy',price: 2500, count: 5, total: 12500},
+    {no: 6,name: 'rice and water',price: 2000, count: 3, total: 6000},
+  ]
 
   constructor(private modalCtrl: ModalController) { }
 
