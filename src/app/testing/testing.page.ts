854eec16fe3598a6817da370a471e9cc875858d0
YuiKon11 <kc4433667@gmail.com>
2022-12-26 15:38:06 +0630

    YuiKon11

--- src/app/testing/testing.page.ts
+++ src/app/testing/testing.page.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import {SiteInformationService} from "../utility/service/siteInformation.service";
+import {ModalController} from "@ionic/angular";
+import {TestingComponent} from "../testingComponent/testing/testing.component";
+
+type productLitType = {
+  Code: string,
+  Name: string,
+  Price: number,
+  Count: number
+}[]
 
 
 @Component({
@@ -7,14 +16,38 @@ import {SiteInformationService} from "../utility/service/siteInformation.service
   templateUrl: './testing.page.html',
   styleUrls: ['./testing.page.scss'],
 })
-export class TestingPage{
+export class TestingPage {
 
-     loaded = true
-
-  constructor(private siteInformationService: SiteInformationService) {
-     console.log(siteInformationService.siteInformation)
-  }
+    productList: productLitType[] = []
 
 
+    loaded = true
 
+    constructor(private siteInformationService: SiteInformationService,
+                private modalCtrl: ModalController) {
+        console.log(siteInformationService.siteInformation)
+    }
+
+    async testingPopUp() {
+
+        const modalCtrl = await this.modalCtrl.create({
+            component: TestingComponent
+        })
+
+
+        modalCtrl.onDidDismiss()
+            .then((data: any) => {
+
+                this.productList.push(data.data)
+
+                this.productList = this.productList.map(m => m)
+                console.log(this.productList)
+
+            }).catch(c => console.log(c))
+
+
+        await modalCtrl.present()
+
+
+    }
 }
