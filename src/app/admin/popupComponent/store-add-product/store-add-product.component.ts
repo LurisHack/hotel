19ea0249e0a7cc5fcf41377c6b069fa09683fa0a
YuiKon11 <kc4433667@gmail.com>
2022-12-26 14:01:56 +0630

    YuiKon11

--- src/app/admin/popupComponent/store-add-product/store-add-product.component.ts
+++ src/app/admin/popupComponent/store-add-product/store-add-product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AdminComponentModule} from "../../component/admin-component.module";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {ModalController} from "@ionic/angular";
 
 @Component({
   standalone: true,
@@ -7,16 +9,36 @@ import {AdminComponentModule} from "../../component/admin-component.module";
   templateUrl: './store-add-product.component.html',
   styleUrls: ['./store-add-product.component.scss'],
   imports: [
-    AdminComponentModule
+    AdminComponentModule,
+    ReactiveFormsModule
   ]
 })
 export class StoreAddProductComponent implements OnInit {
+  formGroup: any;
 
-  constructor() { }
 
-  ngOnInit() {}
+  formInputs = [
+    {name: 'Code', formControlName: 'Code', type: 'text'},
+    {name: 'Name', formControlName: 'Name', type: 'text'},
+    {name:  'Price', formControlName: 'Price', type: 'number'},
+    {name: 'Count', formControlName: 'Count', type: 'number'}
+  ]
+
+
+
+  constructor( private modalCtrl : ModalController) { }
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      Code: new FormControl(null, Validators.required),
+      Name: new FormControl(null, Validators.required),
+      Count: new FormControl(null, Validators.required),
+      Price: new FormControl(null, Validators.required),
+    })
+  }
+
 
     addProduct() {
-        console.log()
+      this.modalCtrl.dismiss( this.formGroup.value).then()
     }
 }
