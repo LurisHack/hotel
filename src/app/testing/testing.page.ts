e993dd644a0e5e141df81b9b7ec9dfb1da7f4335
LurisHack <luris5899@gmail.com>
2022-12-26 12:01:06 +0630

    luris commit

--- src/app/testing/testing.page.ts
+++ src/app/testing/testing.page.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 import {SiteInformationService} from "../utility/service/siteInformation.service";
+import {ModalController} from "@ionic/angular";
+import {TestingComponent} from "../testingComponent/testing/testing.component";
+
+type productLitType = {
+  Code: string,
+  Name: string,
+  Price: number,
+  Count: number
+}[]
 
 
 @Component({
@@ -9,12 +18,38 @@ import {SiteInformationService} from "../utility/service/siteInformation.service
 })
 export class TestingPage{
 
-     loaded = true
+  productList: productLitType[] = []
 
-  constructor(private siteInformationService: SiteInformationService) {
+
+  loaded = true
+
+  constructor(private siteInformationService: SiteInformationService,
+              private modalCtrl: ModalController) {
      console.log(siteInformationService.siteInformation)
   }
 
+    async testingPopUp(){
 
+
+       const modalCtrl = await this.modalCtrl.create({
+         component: TestingComponent
+       })
+
+   modalCtrl.onDidDismiss()
+       .then((data: any) => {
+
+         this.productList.push(data.data)
+
+        this.productList = this.productList.map(m => m)
+         console.log(this.productList)
+
+       })
+
+
+
+      await modalCtrl.present()
+
+
+}
 
 }
