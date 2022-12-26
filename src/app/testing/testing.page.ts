60e0e01182481c1d986255c8d7e6b20cadc93743
Fujitsu <linhtetkyaw1478@gmail.com>
2022-12-26 14:51:07 +0630

    ROBERTgj

--- src/app/testing/testing.page.ts
+++ src/app/testing/testing.page.ts
@@ -1,14 +1,5 @@
 import { Component } from '@angular/core';
 import {SiteInformationService} from "../utility/service/siteInformation.service";
-import {ModalController} from "@ionic/angular";
-import {TestingComponent} from "../testingComponent/testing/testing.component";
-
-type productLitType = {
-  Code: string,
-  Name: string,
-  Price: number,
-  Count: number
-}[]
 
 
 @Component({
@@ -18,38 +9,12 @@ type productLitType = {
 })
 export class TestingPage{
 
-  productList: productLitType[] = []
+     loaded = true
 
-
-  loaded = true
-
-  constructor(private siteInformationService: SiteInformationService,
-              private modalCtrl: ModalController) {
+  constructor(private siteInformationService: SiteInformationService) {
      console.log(siteInformationService.siteInformation)
   }
 
-    async testingPopUp(){
 
-       const modalCtrl = await this.modalCtrl.create({
-         component: TestingComponent
-       })
-
-
-      modalCtrl.onDidDismiss()
-       .then((data: any) => {
-
-         this.productList.push(data.data)
-
-        this.productList = this.productList.map(m => m)
-         console.log(this.productList)
-
-       }).catch(c => console.log(c))
-
-
-
-      await modalCtrl.present()
-
-
-}
 
 }
