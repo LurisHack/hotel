855ca0af66b58680e64597527d0ed4921cbedcea
LurisHack <luris5899@gmail.com>
2022-12-30 21:47:17 +0630

    luris commit

---
+++ src/app/utility/service/storage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {IonicStorageModule, Storage} from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class StorageService {
+
+  createdStorage: any
+
+  constructor(private storage: Storage) {
+    this.storage.create().then((storage:any) => {
+      this.createdStorage = storage
+    })
+      .catch(c => console.log('Storage error ', c))
+  }
+
+  setStorage(){
+    this.createdStorage.set()
+  }
+
+}
