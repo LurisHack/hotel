373e1ccc5805303c1a0d1e025718eae7a77b46a5
LurisHack <luris5899@gmail.com>
2022-12-31 14:46:28 +0630

    luris commit

--- src/app/admin/component/setting/setting.component.ts
+++ src/app/admin/component/setting/setting.component.ts
@@ -16,6 +16,10 @@ export class SETTINGComponent{
     {header: 'Welcome Presentation', subHeader: 'Change hotel detail text, photo', link: '/admin-home/ROOM' , img: '/assets/admin/welcome_presentation.jpg'},
     {header: 'Room Type', subHeader: 'Special, Normal, Deluxe, Luxury', link: '/admin-home/roomType' , img: '/assets/admin/room_type.jpg'},
     {header: 'Room List', subHeader: 'Building list, Room list, Room number', link: '/admin-home/roomList' , img: '/assets/admin/room_list.jpg'},
+    {header: 'Playground', subHeader: 'Swimming pool, Riding, Racing etc... add to playground', link: '/admin-home/playGround' , img: '/assets/admin/playground.jpg'},
+    {header: 'Roof Top Bar', subHeader: 'Adding table for roof top bar and restaurant', link: '/admin-home/roofTopBar' , img: '/assets/admin/rooftopbar.jpg'},
+    {header: 'Salary Package', subHeader: 'Creating salary package depend on user role', link: '/admin-home/salaryPackage' , img: '/assets/admin/salary_package.jpg'},
+
   ]
 
 }
